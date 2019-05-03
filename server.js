b27592076cfcf2757caa8dff1c0a1608e1bca0d9
LuzHN <coldblitz@unitec.edu>
2019-05-02 18:04:04 -0600

    test de permisos

--- server.js
+++ server.js
@@ -20,15 +20,15 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
+/*
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS , PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
-
+*/
 var Schema = mongo.Schema;
 
 var MovieSchema = new Schema({
